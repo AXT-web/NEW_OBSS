da6901ca6bef6f43c13d23775b604128cdfb4dd1
AXT-web <m18775921835@163.com>
2021-05-10 18:26:22 +0800

    VUE:完成了Login页面和vue部分结构

---
+++ Vue/new_obss_vue/src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      <header></header>
+      
+      <footer></footer>
+    </div>
+</template>
+
+<script>
+export default {
+  methods: {
+  }
+}
+</script>
+
+<style lang='less' scoped>
+</style>
---
+++ Vue/new_obss_vue/src/main.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router/router'
+import './plugins/element.js'
+import 'element-ui/lib/theme-chalk/index.css'
+import './assets/css/global.css'
+import './assets/fonts/iconfont.css'
+import axios from 'axios'
+
+Vue.config.productionTip = false
+
+axios.defaults.baseURL = 'http://127.0.0.1:3333/'
+Vue.prototype.$http = axios
+
+// //请求在到达服务器之前，先会调用use中的这个回调函数来添加请求头信息
+// axios.interceptors.request.use(config=>{
+//   //为请求头对象，添加token验证的Authorization字段
+//   config.headers.Authorization = window.sessionStorage.getItem("token")
+//   return config
+// })
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
